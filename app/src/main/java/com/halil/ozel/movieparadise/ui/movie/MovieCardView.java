1907599eac3191e195fa4073dcfeb94ed133156c
Halil Özel <halilozel1903@gmail.com>
2023-05-07 00:36:42 +0300

    ButterKnife removed for MovieCardView

--- app/src/main/java/com/halil/ozel/movieparadise/ui/movie/MovieCardView.java
+++ app/src/main/java/com/halil/ozel/movieparadise/ui/movie/MovieCardView.java
@@ -1,36 +1,25 @@
 package com.halil.ozel.movieparadise.ui.movie;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-
 import com.halil.ozel.movieparadise.R;
 import com.halil.ozel.movieparadise.dagger.modules.HttpClientModule;
 import com.halil.ozel.movieparadise.data.models.Movie;
 import com.halil.ozel.movieparadise.ui.base.BindableCardView;
 
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-
 public class MovieCardView extends BindableCardView<Movie> {
 
-    @SuppressLint("NonConstantResourceId")
-    @BindView(R.id.poster_iv)
     ImageView mPosterIV;
-
-    @SuppressLint("NonConstantResourceId")
-    @BindView(R.id.title_tv)
     TextView title_tv;
 
     public MovieCardView(Context context) {
         super(context);
-        ButterKnife.bind(this);
+        mPosterIV = findViewById(R.id.poster_iv);
+        title_tv = findViewById(R.id.title_tv);
     }
 
     @Override
