974fc5b216b12208e8236f93a4b6ff81816c3ffb
Halil Özel <halilozel1903@gmail.com>
2023-05-07 00:37:08 +0300

    Code reformat made.

--- app/src/main/java/com/halil/ozel/movieparadise/data/models/Movie.java
+++ app/src/main/java/com/halil/ozel/movieparadise/data/models/Movie.java
@@ -3,6 +3,8 @@ package com.halil.ozel.movieparadise.data.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import androidx.annotation.Nullable;
+
 import com.squareup.moshi.Json;
 
 import java.util.List;
