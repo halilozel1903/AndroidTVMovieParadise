1558ed4b56357d2e7845d430061d38b1b3b0814f
Halil Ozel <halilozel1903@gmail.com>
2026-05-27 19:11:51 +0300

    Remove unused setters and ID from Video model

--- app/src/main/java/com/halil/ozel/movieparadise/data/models/Video.java
+++ app/src/main/java/com/halil/ozel/movieparadise/data/models/Video.java
@@ -11,48 +11,19 @@ public class Video {
     public Video() {
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public Video setId(String id) {
-        this.id = id;
-        return this;
-    }
-
     public String getKey() {
         return key;
-    }
-
-    public Video setKey(String key) {
-        this.key = key;
-        return this;
     }
 
     public String getSite() {
         return site;
     }
 
-    public Video setSite(String site) {
-        this.site = site;
-        return this;
-    }
-
     public String getType() {
         return type;
     }
 
-    public Video setType(String type) {
-        this.type = type;
-        return this;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public Video setName(String name) {
-        this.name = name;
-        return this;
     }
 }
